48af72b52cae0e54c4250cf3daa382fb1da33050
Neto test <gnusov87@yandex.ru>
2026-04-02 18:48:41 +0300

    Fix CI: add Chrome installation and headless mode

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,14 +1,8 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,7 +10,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardOrderTest {
@@ -31,7 +24,11 @@ public class CardOrderTest {
     @BeforeEach
     public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); // Для CI
+        options.addArguments("--window-size=1920,1080");
+
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
@@ -47,18 +44,14 @@ public class CardOrderTest {
     public void positiveTest() {
         driver.get("http://localhost:9999");
 
-        // Заполняем поля
         driver.findElement(By.name("name")).sendKeys("Иванов Иван");
         driver.findElement(By.name("phone")).sendKeys("+79001234567");
 
-        // Клик по чекбоксу через JavaScript
         WebElement checkbox = driver.findElement(By.name("agreement"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkbox);
 
-        // Клик по кнопке
         driver.findElement(By.className("button")).click();
 
-        // Ждем и проверяем сообщение об успехе
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("App_appContainer__3jRx1")));
 
         String actualText = driver.findElement(By.className("App_appContainer__3jRx1")).getText();
@@ -72,14 +65,11 @@ public class CardOrderTest {
         driver.findElement(By.name("name")).sendKeys("Ivan Ivanov");
         driver.findElement(By.name("phone")).sendKeys("+79001234567");
 
-        // Клик по чекбоксу через JavaScript
         WebElement checkbox = driver.findElement(By.name("agreement"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkbox);
 
-        // Клик по кнопке
         driver.findElement(By.className("button")).click();
 
-        // Ждем сообщение об ошибке
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test-id='name'] .input__sub")));
 
         WebElement errorMessage = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub"));
@@ -93,14 +83,11 @@ public class CardOrderTest {
         driver.findElement(By.name("name")).sendKeys("Иванов Иван");
         driver.findElement(By.name("phone")).sendKeys("+790012345");
 
-        // Клик по чекбоксу через JavaScript
         WebElement checkbox = driver.findElement(By.name("agreement"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkbox);
 
-        // Клик по кнопке
         driver.findElement(By.className("button")).click();
 
-        // Ждем сообщение об ошибке
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test-id='phone'] .input__sub")));
 
         WebElement errorMessage = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
